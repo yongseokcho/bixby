1bd09922f7c8af255c901e7879bd835173872f1e
MUN SEONG UK <gukbab1216@gmail.com>
2019-06-10 23:38:04 +0900

    213

--- ssafy_1th_gumi_3.bixef/code/test.js
+++ ssafy_1th_gumi_3.bixef/code/test.js
@@ -1,3 +1,10 @@
-module.exports.function = function test (arg1, arg2, ...) {
-  return {}
+module.exports.function = function test () {
+  let recipe = {
+    name: "김창윤의 후루룹",
+    image: {url: "https://i.ytimg.com/vi/5r1oAKWNk6E/maxresdefault.jpg"},
+    rating: 0,
+    hit: 100
+  }
+
+  return recipe
 }
