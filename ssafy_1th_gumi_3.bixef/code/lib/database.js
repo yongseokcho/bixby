7fcd2c550236fb1d10fca7115e6445698bae464d
BeautifulBeer <dbsrhksdnd@gmail.com>
2019-06-22 00:36:05 +0900

    exercise integration

--- ssafy_1th_gumi_3.bixef/code/lib/database.js
+++ ssafy_1th_gumi_3.bixef/code/lib/database.js
@@ -1,50 +1,50 @@
 module.exports.sports = [
     {
       title: "테니스",
-      calorie: 200,
-      time: 40,
+      calorie: 588,
+      time: 0,
       url: "/img/tennis-64.png"
     },
     {
-      title: "러닝",
-      calorie: 300,
-      time: 30,
+      title: "러닝(10km/h)",
+      calorie: 735,
+      time: 0,
       url: "/img/running-64.png"
     },
     {
       title: "축구",
-      calorie: 250,
-      time: 20,
+      calorie: 588,
+      time: 0,
       url: "/img/football-64.png"
     },
     {
       title: "탁구",
-      calorie: 250,
-      time: 20,
+      calorie: 294,
+      time: 0,
       url: "/img/ping-pong-64.png"
     },
     {
       title: "줄넘기",
-      calorie: 400,
-      time: 40,
+      calorie: 735,
+      time: 0,
       url: "/img/skipping-rope-64.png"
     },
     {
       title: "헬스",
-      calorie: 400,
-      time: 40,
+      calorie: 588,
+      time: 0,
       url: "/img/fitness-64.png"
     },
     {
       title: "에어로빅",
-      calorie: 400,
-      time: 40,
+      calorie: 514,
+      time: 0,
       url: "/img/zumba-64.png"
     },
     {
-      title: "수영",
-      calorie: 400,
-      time: 40,
+      title: "수영(평영)",
+      calorie: 585,
+      time: 0,
       url: "/img/swimmer-64.png"
     }
   ]
