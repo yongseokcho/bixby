c612345d3942c7ef5da6bebcd1c2521cc1ab235f
BeautifulBeer <dbsrhksdnd@gmail.com>
2019-07-01 22:11:47 +0900

    bixby final version 0701

--- ssafy_1th_gumi_3.bixef/code/guide/ShowGuideLine.js
+++ ssafy_1th_gumi_3.bixef/code/guide/ShowGuideLine.js
@@ -1,4 +1,4 @@
-module.exports.function = function showGuideLine (guideKeyword){
+module.exports.function = function showGuideLine (guideKeyword, searchKeyword){
   var db = require('lib/database.js');
   return db.guides;
 }
